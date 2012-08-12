696a392a8d5bb8edfcce79261cb0862ef1c2270f
Sean Gooding <sgooding@gmail.com>
2012-08-12 09:46:34 -0600

    added wild thumper controller software

---
+++ Wild_Thumper_Controller/Wild_Thumper_Controller/IOpins.h
@@ -0,0 +1,22 @@
+
+#define LmotorA             3  // Left  motor H bridge, input A
+#define LmotorB            11  // Left  motor H bridge, input B
+#define RmotorA             5  // Right motor H bridge, input A
+#define RmotorB             6  // Right motor H bridge, input B
+
+#define RCleft              2  // Digital input 0
+#define RCright             4  // Digital input 1
+
+//#define S0                  0  // Servo output 00
+//#define S1                  1  // Servo output 01
+//#define S2                  7  // Servo output 02
+#define S3                  8  // Servo output 03
+#define S4                  9  // Servo output 04
+#define S5                 10  // Servo output 05
+#define S6                 12  // Servo output 06
+
+#define Battery             0  // Analog input 00
+#define RmotorC             6  // Analog input 06
+#define LmotorC             7  // Analog input 07
+#define Charger            13  // Low=ON High=OFF
+#define toggleAuto          7  // d7 1 left-left/right, 
